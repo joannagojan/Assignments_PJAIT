8e6f292295fbc3811458ecc5a1ded125dc2efe4e
JoannaGoj <joannagojan@gmail.com>
2025-04-27 17:51:44 +0800

    password task in progress

--- C++/task5/gojan.cpp
+++ C++/task5/gojan.cpp
@@ -2,39 +2,42 @@
 #include <string>
 #include <cctype>
 #include <vector>
+#include <functional>
 
 
 
 class ValidatorAbstract {
     public: 
-    virtual bool validateText(std::string text;);
-}
+    virtual bool validate(const std::string& text) const = 0;
+    virtual std::string getError() const = 0;
+};
 
-class ValidateOccurences {
-    public: 
-    int target;
-    ValidatorAbstract validator;
+class ValidateOccurences : ValidatorAbstract {
+    public:
+    ValidateOccurences(std::function<bool(char)> condition, int target) : 
+    condition(condition), targer(target) {validate(target)};
 
-    ValidateOccurences(int target, ValidatorAbstract validator, std::string text) : target(target), validator(validator) {
-        validateTarget(target)
+
+    bool validate(const std::string& text) override {
+        int counter = 0;
+        for (auto c: text) {
+            if (condition(c)) {counter++;};
+        }
+        return counter >= target;
     }
-    countOccurences();
 
     private:
-    bool validateTarget(int target) [
-        return target >= 0;
-    ]
+        std::function<bool(char)> condition;
+        int target;
 
-}
+        bool validate(int target) {
+            if (target <= 0) {
+                throw new std::illegal_argument("The counter target is cannot not positive")
+            };
+        }
+};
 
-class ValidatePassword {
-    public: 
-    std::vector<ValidateOccurences> 
-}
 
-class ValidateAlpha : class ValidatorAbstract {
-    validateText()
-}
 
 bool isLetter(char c) {
     return std::isalpha(c);
@@ -69,28 +72,24 @@ bool isCharDistinct(const std::string& text, int charIndex) {
 
 
 bool checkpass(const std::string& password) {
-
-
+    return true;
 }
 
 
 int main() {
-    std::cout << isLetter('3') << '\n';
-
-using std::cout; using std::endl;
 
 
-const std::string passes[] =
-{
-"AbcDe93", "A1b:A1b>", "Ab:Acb<",
-"abc123><", "Zorro@123"
-};
+
+// const std::string passes[] =
+// {
+// "AbcDe93", "A1b:A1b>", "Ab:Acb<",
+// "abc123><", "Zorro@123"
+// };
 
 
-for (size_t i = 0; i < std::size(passes); ++i) {
-cout << "checking " << passes[i] << endl;
-if (checkpass(passes[i])) cout << "OK" << endl;
-cout << endl;
-
-    }
+// for (size_t i = 0; i < std::size(passes); ++i) {
+// cout << "checking " << passes[i] << endl;
+// if (checkpass(passes[i])) cout << "OK" << endl;
+// cout << endl;
+    // }
 }
